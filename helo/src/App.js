79847ec4051c197cceb0de1f568897b2a5e2535e
karttofel <jhornancolina@gmail.com>
2019-09-07 10:18:03 -0500

    Fix: quite yarn and npm tutorial of the readme only leaving npx, fixing problem with dependencies

---
+++ helo/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+const Rocket = () => {
+  return(
+    <h1 className='icon'>🚀</h1>
+  )
+}
+function App() {
+  return (
+    <div className="App">
+      <div className="box">
+        <Rocket/>
+      </div>
+      <h1 className='title'>Parcel</h1>
+      <h1 className='sub-title'>To get started, edit <span>src/App.js</span> and save to reaload.</h1>
+    </div>
+  );
+}
+export default App
