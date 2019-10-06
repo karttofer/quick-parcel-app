d43661c72eb90b36bd665bea27063e2ac6764d5b
karttofel <jhornancolina@gmail.com>
2019-10-06 10:16:29 -0500

    Run: prettier

--- src/cli.js
+++ src/cli.js
@@ -6,13 +6,14 @@
 
 */
 import shell from 'shelljs'
-import path  from 'path'
-import { welcomeMessage, finalMessage ,taskList } from './methods.js' 
-const actualPath = import.meta.url;
-const templatesDir = path.resolve(new URL(actualPath).pathname,'../../templates/default',
-).slice(3)
+import path from 'path'
+import { welcomeMessage, finalMessage, taskList } from './methods.js'
+const actualPath = import.meta.url
+const templatesDir = path
+  .resolve(new URL(actualPath).pathname, '../../templates/default')
+  .slice(3)
 export async function cli(args) {
-	welcomeMessage(args,`${process.cwd()}/${args}`);
-	await taskList(templatesDir,`${process.cwd()}/${args}`, args);
-	finalMessage(args,`${process.cwd()}`);
+  welcomeMessage(args, `${process.cwd()}/${args}`)
+  await taskList(templatesDir, `${process.cwd()}/${args}`, args)
+  finalMessage(args, `${process.cwd()}`)
 }
